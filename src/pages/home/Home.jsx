2472b1a8c60c60b58ea83d0b664002a7c0933537
Chirag_Berde <117891629+Chirag-Berde@users.noreply.github.com>
2023-06-29 21:29:12 +0530

    working in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './home.scss'
 import Herobanner from './herobanner/Herobanner'
+import Trending from './trending/trending'
 
 const Home = () => {
   return (
     <div className='homePage'>
       <Herobanner />
+      <Trending/>
       <div style={{height:1000}}></div>
     </div>
   )
