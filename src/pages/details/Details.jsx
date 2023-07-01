7dff1c0045914efe86342e1c8c3e28b9f00e3cd1
Chirag_Berde <117891629+Chirag-Berde@users.noreply.github.com>
2023-07-01 21:48:55 +0530

    issue in span, director name is not getting from api in detailsBanner comp

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import './details.scss'
+import './details.scss' 
 import useFetch from '../../hooks/useFetch'
 import { useParams } from 'react-router-dom'
 import DetailsBanner from './detailsBanner/DetailsBanner'
 
 
 const Details = () => {
-  // const {mediaType,id}=useParams()
-  // const {data,loading}=useFetch(`/${mediaType}/${id}`)
+  const {mediaType,id}=useParams()
+  const {data,loading}=useFetch(`/${mediaType}/${id}/videos`)
+  const {data:credits,loading:creditsLoading}=useFetch(`/${mediaType}/${id}/credits`)
   return (
-    <div><DetailsBanner/></div>
+    <div><DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/></div>
   )
 }
 
--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -9,12 +9,16 @@ import Genres from "../../../components/genres/Genres";
 import CircleRating from "../../../components/circlerating/CircleRating";
 import Img from "../../../components/lazyloadimage/Img";
 import PosterFallback from "../../../assets/no-poster.png";
+import { Playbutton } from "../Playbutton";
 
 const DetailsBanner = ({ video, crew }) => {
     const { mediaType, id } = useParams()
     const { data, loading } = useFetch(`/${mediaType}/${id}`)
     const { url } = useSelector(state => state.home)
-
+    const _genres = data?.genres.map(item => item.id)
+    const director = crew?.filter(item => item.job === "Director")
+    const writer = crew?.filter(item => item.job === "Screenplay" || item.job === "Story" || item.job === "Writer")
+    console.log(director)
     const toHoursAndMinutes = (totalMinutes) => {
         const hours = Math.floor(totalMinutes / 60);
         const minutes = totalMinutes % 60;
@@ -36,7 +40,75 @@ const DetailsBanner = ({ video, crew }) => {
                                         {data.poster_path ? (<Img className="posterImg" src={url.backdrop + data.poster_path} />) : (<Img className="posterImg" src={PosterFallback} />)}
                                     </div>
                                     <div className="right">
+                                        <div className="title">
+                                            {`${data.name || data.title} (${dayjs(data?.release_date).format("YYYY")})`}
+                                        </div>
+                                        <div className="subtitle">
+                                            {data.tagline}
+                                        </div>
+                                        <Genres data={_genres} />
+                                        <div className="row">
+                                            <CircleRating rating={data.vote_average.toFixed(1)} />
+                                            <div className="playbtn" onclick={() => { }}>
+                                                <Playbutton /> Watch Trailer
+                                            </div>
+                                        </div>
+                                        <div className="overview">
+                                            <div className="heading">
+                                                Overview
+                                            </div>
+                                            <div className="description">
+                                                {data.overview}
+                                            </div>
+                                        </div>
+                                        <div className="info">
+                                            {data.status && (
+                                                <div className="infoItem">
+                                                    <span className="text bold">
+                                                        Status: {" "}
+                                                    </span>
+                                                    <span className="text">
+                                                        {data.status}
+                                                    </span>
+                                                </div>
+                                            )}
+                                            {data.release_date && (
+                                                <div className="infoItem">
+                                                    <span className="text bold">
+                                                        Release Date: {" "}
+                                                    </span>
+                                                    <span className="text">
+                                                        {dayjs(data.release_date).format("MMM D, YYYY")}
+                                                    </span>
+                                                </div>
+                                            )}
+                                            {data.runtime && (
+                                                <div className="infoItem">
+                                                    <span className="text bold">
+                                                        Release Date: {" "}
+                                                    </span>
+                                                    <span className="text">
+                                                        {toHoursAndMinutes(data.runtime)}
+                                                    </span>
+                                                </div>
+                                            )}
+                                        </div>
+                                        {director?.length > 0 && (
+                                            <div className="info">
+                                                <span className="text bold">
+                                                    Director :{" "}
+                                                </span>
+                                                <span>
+                                                    {director.map((d, index) => {
+                                                        <span key={index}>
+                                                            {d.name}
+                                                            {director.length - 1 !== index && ", "}
+                                                        </span>
 
+                                                    })}
+                                                </span>
+                                            </div>
+                                        )}
                                     </div>
                                 </div>
                             </ContentWrapper>
