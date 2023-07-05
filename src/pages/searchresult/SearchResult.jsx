7f9bc4791a976993d224cf3df08dd9ef8435fc2e
Chirag_Berde <117891629+Chirag-Berde@users.noreply.github.com>
2023-07-05 21:28:22 +0530

    searchResult component completed & error fixed

--- src/pages/searchresult/SearchResult.jsx
+++ src/pages/searchresult/SearchResult.jsx
@@ -6,6 +6,7 @@ import { fetchData } from '../../utils/api';
 import ContentWrapper from '../../components/contentWrapper/ContentWrapper';
 import noResults from "../../assets/no-results.png"
 import Spinner from "../../components/spinner/Spinner"
+import MovieCard from '../../components/moviecard/MovieCard';
 
 const SearchResult = () => {
   const [data, setData] = useState(null)
@@ -24,7 +25,7 @@ const SearchResult = () => {
   const fetchNextPageData = () => {
     fetchData(`/search/multi?query=${query}&page=${pageNum}`).then(response => {
       if (data?.results) {
-        setData({ ...data, results: [...data.results, ...response.results] })
+        setData({ ...data, results: [...data?.results, ...response.results] })
       } else {
         setData(response)
       }
@@ -33,12 +34,36 @@ const SearchResult = () => {
   }
 
   useEffect(() => {
+    setPageNum(1)
     fetchInitialData()
   }, [query])
   return (
     <div className='searchResultsPage'>
-      {loading && <Spinner initial={true}/>}
-    </div>
+      {loading && <Spinner initial={true} />} 
+      {!loading && (
+        <ContentWrapper>
+          {data?.results?.length > 0 ? (
+            <>
+              <div className="pageTitle">
+                {`Search ${data?.total_results > 1 ? "results" : "result"} of '${query}'`}
+              </div>
+              <InfiniteScroll className='content' dataLength={data?.results?.length || []} next={fetchNextPageData} hasMore={pageNum <= data?.total_pages} loader={<Spinner />}>
+                {data?.results?.map((item, index) => {
+                  if (item.media_type === "person") return;
+                    return (<MovieCard key={index} data={item} fromSearch={true} />)
+                  
+
+                })}
+              </InfiniteScroll>
+            </>) : (
+            <span className="resultNotFound">
+              Sorry, Results not found !!
+            </span>
+          )}
+        </ContentWrapper>
+      )
+      }
+    </div >
   )
 }
 
