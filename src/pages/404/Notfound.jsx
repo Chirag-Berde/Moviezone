5dafcff75c7da9db3b5433fbd4f5fbed606ec0fd
Chirag_Berde <117891629+Chirag-Berde@users.noreply.github.com>
2023-07-06 21:42:41 +0530

    image added, css work remaining in notfound component

--- src/pages/404/Notfound.jsx
+++ src/pages/404/Notfound.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './notfound.scss'
+import notfoundImg from "./notfoundImg.png"
 
 const Notfound = () => {
   return (
-    <div>Notfound</div>
+    <>
+    <div className="title">Oops !!</div>
+      <div className='notfoundDiv'>
+
+        <img src={notfoundImg} alt="notFound" />
+      </div>
+    </>
+
+
+
   )
 }
 
